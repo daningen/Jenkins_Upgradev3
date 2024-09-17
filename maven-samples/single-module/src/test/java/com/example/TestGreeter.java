0247e2824cb8b8d3bbebb8f138e6c2d37e0ac425
daning <dan.erlandsson@gmail.com>
2024-09-17 20:27:27 +0200

    Update TestGreeter.java
    add failtest on main

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,6 +26,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
+   @Test
+  public void intentionallyFailed() {
+    assertTrue(false));
+  }
   
 
   @Test
