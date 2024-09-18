690be7529602b01224a5c5e116ca03cfcd8cc17e
daning <dan.erlandsson@gmail.com>
2024-09-18 21:06:28 +0200

    removed failing test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,10 +26,7 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-   @Test
-  public void intentionallyFailed() {
-    assertTrue(true);
-  }
+   
   
 
   @Test
